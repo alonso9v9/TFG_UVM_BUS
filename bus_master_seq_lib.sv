24061f2ee10c6c6c108365641473d90221d12342
Luis Alonso Vega <alonso9v9@gmail.com>
2021-10-12 22:58:31 -0600

    umv_do

--- bus_master_seq_lib.sv
+++ bus_master_seq_lib.sv
@@ -61,9 +61,7 @@ class random_sequence extends bus_base_sequence;
     virtual task body();
         $display("SEQUENCE BODY");
         repeat (2) begin
-            `uvm_do_with(req, 
-            { req.tipo == trans;
-            req.retardo == retardo;} )
+            `uvm_do(req)
             //get_response(rsp);
             $display("[SEQUENCE] Item sent to driver");
             `uvm_info(get_type_name(), $sformatf("SEQUENCE item sent"), UVM_HIGH);
