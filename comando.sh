eea232f3e64b39b41fa416216a1e651a31e43143
Luis Alonso Vega <alonso9v9@gmail.com>
2021-11-10 21:23:46 -0600

    Cambios en comando

--- comando.sh
+++ comando.sh
@@ -1,7 +1,6 @@
-source /mnt/vol_NFS_rh003/estudiantes/archivos_config/synopsys_tools.sh
 vcs -Mupdate testbench.sv  -o salida  -full64 -sverilog  -kdb -debug_acc+all -debug_region+cell+encrypt -l log_test -ntb_opts uvm-1.2 +lint=TFIPC-L -cm line+tgl+cond+fsm+branch+assert -cm_tgl assign+portsonly+fullintf+mda+count+structarr -lca
 ./salida -cm line+tgl+cond+fsm+branch+assert +ITER=500 +UVM_TIMEOUT=100000 > run_log
-./salida -cm line+tgl+cond+fsm+branch+assert +UVM_TESTNAME="max_alter_test" +ITER=500 +UVM_TIMEOUT=100000>run_log_alter 
-./salida -cm line+tgl+cond+fsm+branch+assert +UVM_TESTNAME="dest_alter_test" +ITER=500 +UVM_TIMEOUT=100000>run_log_dest 
-./salida -cm line+tgl+cond+fsm+branch+assert +UVM_TESTNAME="orig_alter_test" +ITER=500 +UVM_TIMEOUT=100000> run_log_orig 
-./salida -cm line+tgl+cond+fsm+branch+assert +UVM_TESTNAME="bursts_test" +ITER=30 +BURSTS=10 +UVM_TIMEOUT=100000> run_log_burst
+./salida -cm line+tgl+cond+fsm+branch+assert -seed $1 +UVM_TESTNAME="max_alter_test" +ITER=500 +UVM_TIMEOUT=100000>run_log_alter 
+./salida -cm line+tgl+cond+fsm+branch+assert -seed $1 +UVM_TESTNAME="dest_alter_test" +ITER=500 +UVM_TIMEOUT=100000>run_log_dest 
+./salida -cm line+tgl+cond+fsm+branch+assert -seed $1 +UVM_TESTNAME="orig_alter_test" +ITER=500 +UVM_TIMEOUT=100000> run_log_orig 
+./salida -cm line+tgl+cond+fsm+branch+assert -seed $1 +UVM_TESTNAME="bursts_test" +ITER=30 +BURSTS=10 +UVM_TIMEOUT=100000> run_log_burst
--- comando_corridas.sh
+++ comando_corridas.sh
@@ -1,24 +1,16 @@
-printf "Drivers,Fifo Depth,Max Bandwidth(Mbps)" > max_bandwidth.csv
-printf "Drivers,Fifo Depth,Min Bandwidth(Mbps)" > min_bandwidth.csv
 source /mnt/vol_NFS_rh003/estudiantes/archivos_config/synopsys_tools.sh
-
-for pckg in  32  
+for bits in {16..32..2} 
 	do
-	for drvrs in 2 4 6 12 	
+	for drvrs in 2 4 8 16 	
 	do
-		for fifo in 16 32 64 256 
+		for seed in  {1..100}
 		do
-			printf "\x60define SCRIPT 1 \n" > parameters.sv
-			printf "parameter pckg_sz = %d;\nparameter drvrs = %d;\n parameter fifo_depth=%d;\n" $pckg $drvrs $fifo  >> parameters.sv
-			vcs -Mupdate parameters.sv testbench.sv  -o salida  -full64 -sverilog  -kdb -debug_acc+all -debug_region+cell+encrypt -l log_test +lint=TFIPC-L
-			./salida
+			echo "package bus_parameters;\nparameter bits=$bits;\nparameter drvrs=$drvrs;\n parameter fif_Size=1000;\nparameter buses=1;\nendpackage:bus_parameters" > bus_parameters.sv
+			./comando.sh $seed
 			if $?; then
-				printf "\n\n\n\n\nError in run drvrs=%d, pckg_sz=%d, fifo_depth=%d\n\n\n\n" $pckg $drvrs $fifo
+				echo "\n\n\n\n\nError in run bits=$bits, drvrs=$drvrs"
 				return 1
 			fi
 		done
 	done
 done
-
-gnuplot -p min.gnuplot
-gnuplot -p max.gnuplot
