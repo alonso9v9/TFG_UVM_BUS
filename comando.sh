58193e11a736a163aa90e90e20d88d5d9aa1460e
Luis Alonso Vega <alonso9v9@gmail.com>
2021-11-11 00:51:59 -0600

    Cambios

--- comando.sh
+++ comando.sh
@@ -1,5 +1,5 @@
-./salida_${1}_${2} -cm line+tgl+cond+fsm+branch+assert -seed $3 +ITER=50 +UVM_TIMEOUT=10000 > /dev/null
-./salida_${1}_${2} -cm line+tgl+cond+fsm+branch+assert -seed $3 +UVM_TESTNAME="max_alter_test" +ITER=15 +UVM_TIMEOUT=10000 > /dev/null
-./salida_${1}_${2} -cm line+tgl+cond+fsm+branch+assert -seed $3 +UVM_TESTNAME="dest_alter_test" +ITER=50 +UVM_TIMEOUT=10000 > /dev/null
-./salida_${1}_${2} -cm line+tgl+cond+fsm+branch+assert -seed $3 +UVM_TESTNAME="orig_alter_test" +ITER=50 +UVM_TIMEOUT=10000 > /dev/null
-./salida_${1}_${2} -cm line+tgl+cond+fsm+branch+assert -seed $3 +UVM_TESTNAME="bursts_test" +ITER=20 +BURSTS=10 +UVM_TIMEOUT=10000 > /dev/null
+./salida_${1}_${2} -cm line+tgl+cond+fsm+branch+assert -seed $3 +ITER=50 +UVM_TIMEOUT=10000 > run_log
+./salida_${1}_${2} -cm line+tgl+cond+fsm+branch+assert -seed $3 +UVM_TESTNAME="max_alter_test" +ITER=15 +UVM_TIMEOUT=10000 > run_log_alter
+./salida_${1}_${2} -cm line+tgl+cond+fsm+branch+assert -seed $3 +UVM_TESTNAME="dest_alter_test" +ITER=50 +UVM_TIMEOUT=10000 > run_log_dest
+./salida_${1}_${2} -cm line+tgl+cond+fsm+branch+assert -seed $3 +UVM_TESTNAME="orig_alter_test" +ITER=50 +UVM_TIMEOUT=10000 > run_log_orig
+./salida_${1}_${2} -cm line+tgl+cond+fsm+branch+assert -seed $3 +UVM_TESTNAME="bursts_test" +ITER=20 +BURSTS=10 +UVM_TIMEOUT=10000 > run_log_burst
