a582bc8d8bc0f084a00c4fe0f0d901a30f91f982
Luis Alonso Vega <alonso9v9@gmail.com>
2021-11-11 00:43:32 -0600

    Cambios

--- comando_corridas.sh
+++ comando_corridas.sh
@@ -3,12 +3,12 @@ for bits in {16..28..6}
 	do
 	for drvrs in 2 4 8 16 	
 	do
-        vcs -Mupdate testbench.sv  -o salida_$drvrs_$bits  -full64 -sverilog  -kdb -debug_acc+all -debug_region+cell+encrypt -l log_test -ntb_opts uvm-1.2 +lint=TFIPC-L -cm line+tgl+cond+fsm+branch+assert -cm_tgl assign+portsonly+fullintf+mda+count+structarr -lca
+        vcs -Mupdate testbench.sv  -o salida_${drvrs}_${bits}  -full64 -sverilog  -kdb -debug_acc+all -debug_region+cell+encrypt -l log_test -ntb_opts uvm-1.2 +lint=TFIPC-L -cm line+tgl+cond+fsm+branch+assert -cm_tgl assign+portsonly+fullintf+mda+count+structarr -lca
 		for seed in  {1..100}
 		do
             echo "Running seed $seed with bits $bits drvrs $drvrs"
 			echo -e "package bus_parameters;\nparameter bits=$bits;\nparameter drvrs=$drvrs;\nparameter fif_Size=1000;\nparameter buses=1;\nendpackage:bus_parameters" > bus_parameters.sv
-            ./comando.sh $seed
+            ./comando.sh $drvrs $bits $seed
 
 		done
 	done
